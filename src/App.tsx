284a89fd861cc95abb14346ef6821ebb838fb1bd
Kai A <kailroberts19@gmail.com>
2026-01-21 21:03:17 -0500

    Basic restructure and games dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import LoginPage from './routes/login'
 import './App.css'
+import GamesDashboard from './routes/gamesDashboard'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/games/user/:userId" element={<GamesDashboard />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
